48918c675928272597d1db09585f5db53a285815
Echcharafi Adil <charafi.adil@gmail.com>
2018-04-07 17:28:30 +0100

    Finsalition card game

--- src/App.js
+++ src/App.js
@@ -1,19 +1,83 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Card, GuessCount} from './components/card/Card'
+import {Card} from './components/card/Card'
+import _ from 'lodash'
 
 import HallOfFame, {FAKE_HOF} from  './components/hallframe/HallOfFame'
+import GuessCount from  './components/GuessCount/GuessCount'
+
+
+const SIDE = 2
+const SYMBOLS = ['😀' ,'🎉' ,'💖','🎩', '🐶', '🐱', '🦄' , '🐬' , '🌍' , '🌛', '🌞', '💫', '🍎', '🍌', '🍓', '🍐', '🍟', '🍿']
+const VISUAL_PAUSE_MSECS = 750
 
 
 class App extends Component {
-  // Arrow fx for binding
-  handleCardClick = (card) => {
-    console.log(card, this);
-  }
-  render() {
-    const won = new Date().getSeconds() % 2 === 0
+  state = {
+    cards : this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: []
+  } 
 
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = _.shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return _.shuffle(result)
+  }
+
+  // Arrow fx for binding
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
+  // Arrow fx for binding
+  handleCardClick = (index) => {
+    const { currentPair } = this.state
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+  
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+  
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+    return indexMatched ? 'visible' : 'hidden'
+  }
+
+  render() {
+    const {cards, matchedCardIndices, guesses} = this.state
+    const won = matchedCardIndices.length === cards.length
     return (
       <div className="App">
         <header className="App-header">
@@ -23,14 +87,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">
+          Guess Count : <GuessCount guesses={guesses} />
+        </p>
         <div className="memory">
-          <GuessCount guesses={0} />
-          <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-          <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-          <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-          <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-          <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-          <Card card="🐱" feedback="justMatched"  onClick={this.handleCardClick}  />
+          {cards.map((card, index) => (
+            <Card
+              card={card}
+              feedback={this.getFeedbackForCard(index)}
+              key={index}
+              index={index}
+              onClick={this.handleCardClick}
+            />
+          ))}
         </div>
         <h1>{won && <HallOfFame entries={FAKE_HOF} /> }</h1>
       </div>
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,13 +6,22 @@ import './Card.css';
 
 const HIDDEN_SYMBOL = '❓'
 
-/*class Card extends React.Component {
-    constructor(prop) {
-        super(prop)
+export class Card extends React.Component {
+    static propTypes = {
+        card: PropTypes.string.isRequired,
+        feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'justMismatched',
+        'visible',
+        ]).isRequired,
+        onClick: PropTypes.func.isRequired,
+        index: PropTypes.number.isRequired,
     }
 
     render() {
-        return (<div className={`card ${feedback}`} onClick={() => onClick(card)}>
+        const {card, feedback, onClick, index} = this.props;
+        return (<div className={`card ${feedback}`} onClick={() => onClick(index)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>
@@ -20,33 +29,3 @@ const HIDDEN_SYMBOL = '❓'
         )
     }
 }
-*/
-export const Card = ({card, feedback, onClick}) => 
-    (<div className={`card ${feedback}`} onClick={() => onClick(card)}>
-        <span className="symbol">
-            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
-        </span>
-    </div>)
-
-//Default Values from prop
-/*Card.defaultProps = {
-    feedback: 'hidden'
-}*/
-
-//Validation for prop
-Card.propTypes = {
-    card: PropTypes.string.isRequired,
-    feedback: PropTypes.oneOf([
-      'hidden',
-      'justMatched',
-      'justMismatched',
-      'visible',
-    ]).isRequired,
-    onClick: PropTypes.func.isRequired,
-  }
-
-export const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
-
-GuessCount.propTypes = {
-    guesses: PropTypes.number.isRequired,
-  }
